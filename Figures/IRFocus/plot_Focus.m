73acfb236a551d437ca68c4f481ca02f9a4922c2
rg_laptop <rgeneaux@gmail.com>
2016-05-30 20:16:49 +0200

    Figures lundi

--- Figures/IRFocus/plot_Focus.m
+++ Figures/IRFocus/plot_Focus.m
@@ -1,5 +1,6 @@
 clear all
 A = im2double(rgb2gray(imread('400-800_BBO_min.bmp')));
+load('redcmap.mat');
 
 %%
 figure(1)
@@ -8,3 +9,6 @@ surf(A)
 view(2)
 shading interp
 axis equal
+colormap(cmap)
+a=gca;
+a.CLim = [0.14 0.42];
